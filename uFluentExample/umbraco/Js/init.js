1998b5c42914f320079282a7e0b174b5b3979308
Andy Harding <andyharding04@gmail.com>
2017-03-21 08:54:38 +0000

    Upgrade Umbraco to 7.5.11

--- uFluentExample/umbraco/Js/init.js
+++ uFluentExample/umbraco/Js/init.js
@@ -1,6 +1,6 @@
 /** Executed when the application starts, binds to events and set global state */
-app.run(['userService', '$log', '$rootScope', '$location', 'navigationService', 'appState', 'editorState', 'fileManager', 'assetsService', 'eventsService', '$cookies', '$templateCache',
-    function (userService, $log, $rootScope, $location, navigationService, appState, editorState, fileManager, assetsService, eventsService, $cookies, $templateCache) {
+app.run(['userService', '$log', '$rootScope', '$location', 'navigationService', 'appState', 'editorState', 'fileManager', 'assetsService', 'eventsService', '$cookies', '$templateCache', 'localStorageService',
+    function (userService, $log, $rootScope, $location, navigationService, appState, editorState, fileManager, assetsService, eventsService, $cookies, $templateCache, localStorageService) {
 
         //This sets the default jquery ajax headers to include our csrf token, we
         // need to user the beforeSend method because our token changes per user/login so
@@ -9,10 +9,11 @@ app.run(['userService', '$log', '$rootScope', '$location', 'navigationService', 
             beforeSend: function (xhr) {
                 xhr.setRequestHeader("X-XSRF-TOKEN", $cookies["XSRF-TOKEN"]);
             }
-        }); 
-        
+        });
+
         /** Listens for authentication and checks if our required assets are loaded, if/once they are we'll broadcast a ready event */
         eventsService.on("app.authenticated", function(evt, data) {
+            
             assetsService._loadInitAssets().then(function() {
                 appState.setGlobalState("isReady", true);
 
@@ -24,14 +25,14 @@ app.run(['userService', '$log', '$rootScope', '$location', 'navigationService', 
 
         /** execute code on each successful route */
         $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
-            
+
             if(current.params.section){
                 $rootScope.locationTitle = current.params.section + " - " + $location.$$host;
             }
             else {
                 $rootScope.locationTitle = "Umbraco - " + $location.$$host;
             }
-            
+
             //reset the editorState on each successful route chage
             editorState.reset();
 
@@ -49,25 +50,17 @@ app.run(['userService', '$log', '$rootScope', '$location', 'navigationService', 
             var returnPath = null;
             if (rejection.path == "/login" || rejection.path.startsWith("/login/")) {
                 //Set the current path before redirecting so we know where to redirect back to
-                returnPath = encodeURIComponent($location.url());                                
+                returnPath = encodeURIComponent($location.url());
             }
 
             $location.path(rejection.path)
             if (returnPath) {
                 $location.search("returnPath", returnPath);
             }
-            
+
         });
 
 
-        /** For debug mode, always clear template cache to cut down on 
-            dev frustration and chrome cache on templates */
-        if(Umbraco.Sys.ServerVariables.isDebuggingEnabled){
-            $rootScope.$on('$viewContentLoaded', function() {
-              $templateCache.removeAll();
-            });
-        }
-        
         /* this will initialize the navigation service once the application has started */
         navigationService.init();
 
--- uFluentExample/umbraco_client/Editors/EditView.js
+++ uFluentExample/umbraco_client/Editors/EditView.js
@@ -8,24 +8,6 @@
         //private methods/variables
         _opts: null,
 
-        _updateNewFileProperties: function(filePath) {
-            /// <summary>Updates the current treeSyncPath and original file name to have the new file name</summary>
-            
-            //update the originalFileName prop
-            this._opts.originalFileName = filePath;
-
-            //re-create the new path
-            var subPath = this._opts.treeSyncPath.split(",");
-            //remove the last element
-            subPath.pop();
-            //add the new element
-            var parts = filePath.split("/");
-            //remove the first bit which will either be "Partials" or "MacroPartials"
-            parts.shift();
-            subPath.push(parts.join("/"));
-            this._opts.treeSyncPath = subPath.join();
-        },
-
         // Constructor
         constructor: function (opts) {
             // Merge options with default
@@ -51,24 +33,59 @@
 
         insertMacroMarkup: function(alias) {
             /// <summary>callback used to insert the markup for a macro with no parameters</summary>
-            
+
             UmbEditor.Insert("@Umbraco.RenderMacro(\"" + alias + "\")", "", this._opts.codeEditorElementId);
         },
-        
+
+        insertRenderBody: function() {
+            UmbEditor.Insert("@RenderBody()", "", this._opts.codeEditorElementId);
+        },
+
         openMacroModal: function (alias) {
             /// <summary>callback used to display the modal dialog to insert a macro with parameters</summary>
-            
+
             var self = this;
 
             UmbClientMgr.openAngularModalWindow({
                 template: "views/common/dialogs/insertmacro.html",
                 dialogData: {
                     renderingEngine: "Mvc",
-                    selectedAlias: alias
+                    macroData: {macroAlias: alias}
                 },
                 callback: function (data) {
                     UmbEditor.Insert(data.syntax, '', self._opts.codeEditorElementId);
                 }
+            });
+        },
+
+        openSnippetModal: function (type) {
+            /// <summary>callback used to display the modal dialog to insert a macro with parameters</summary>
+
+            var self = this;
+
+            UmbClientMgr.openAngularModalWindow({
+                template: "views/common/dialogs/template/snippet.html",
+                callback: function (data) {
+
+                    var code = "";
+
+                    if (type === 'section') {
+                        code = "\n@section " + data.name + "{\n";
+                        code += "<!-- Content here -->\n" +
+                            "}\n";
+                    }
+
+                    if (type === 'rendersection') {
+                        if (data.required) {
+                            code = "\n@RenderSection(\"" + data.name + "\", true)\n";
+                        } else {
+                            code = "\n@RenderSection(\"" + data.name + "\", false)\n";
+                        }
+                    }
+
+                    UmbEditor.Insert(code, '', self._opts.codeEditorElementId);
+                },
+                type: type
             });
         },
 
@@ -124,7 +141,7 @@
                     });
             }
             else {
-                //saving a partial view    
+                //saving a partial view
                 var actionName = this._opts.editorType === "PartialViewMacro" ? "SavePartialViewMacro" : "SavePartialView";
 
                 $.post(self._opts.restServiceLocation + actionName,
@@ -142,9 +159,9 @@
                     });
             }
         },
-        
+
         submitSuccess: function (args) {
-            
+
             var msg = args.message;
             var header = args.header;
             var path = this._opts.treeSyncPath;
@@ -155,10 +172,19 @@
                 }
                 path = args.path;
             }
+            if (args.contents) {
+                UmbEditor.SetCode(args.contents);
+            } else if (!this.IsSimpleEditor) {
+                // Restore focuse to text region. SetCode also does this.
+                UmbEditor._editor.focus();
+            }
 
             UmbClientMgr.mainTree().setActiveTreeType(this._opts.currentTreeType);
 
             if (this._opts.editorType == "Template") {
+
+                var alias = args.alias;
+                this._opts.aliasTxtBox.val(alias);
 
                 top.UmbSpeechBubble.ShowMessage('save', header, msg);
 
@@ -171,12 +197,12 @@
                 else {
                     UmbClientMgr.mainTree().syncTree(path, true);
                 }
-                
+
             }
             else {
                 var newFilePath = this._opts.nameTxtBox.val();
 
-               
+
                 function trimStart(str, trim) {
                     if (str.startsWith(trim)) {
                         return str.substring(trim.length);
@@ -196,27 +222,31 @@
                     var newLocation = window.location.pathname + "?" + notFileParts.join("&") + "&file=" + newFilePath;
 
                     UmbClientMgr.contentFrame(newLocation);
-                    
+
                     //we need to do this after we navigate otherwise the navigation will wait unti lthe message timeout is done!
                     top.UmbSpeechBubble.ShowMessage('save', header, msg);
                 }
                 else {
-                    
+
                     top.UmbSpeechBubble.ShowMessage('save', header, msg);
 
-                    //then we need to update our current tree sync path to represent the new one
-                    this._updateNewFileProperties(newFilePath);
+                    if (args && args.name) {
+                        this._opts.originalFileName = args.name;
+                    }
+                    if (args && args.path) {
+                        this._opts.treeSyncPath = args.path;
+                    }
 
                     UmbClientMgr.mainTree().syncTree(path, true, null, newFilePath.split("/")[1]);
-                }                
+                }
             }
-            
+
         },
-        
+
         submitFailure: function (err, header) {
-            top.UmbSpeechBubble.ShowMessage('error', header, err);                       
+            top.UmbSpeechBubble.ShowMessage('error', header, err);
         },
-        
+
         changeMasterPageFile: function ( ) {
             //var editor = document.getElementById(this._opts.sourceEditorId);
             var templateDropDown = this._opts.masterPageDropDown.get(0);
@@ -252,7 +282,7 @@
     $.ajaxSetup({
         dataType: 'json',
         cache: false,
-        contentType: 'application/json; charset=utf-8'        
+        contentType: 'application/json; charset=utf-8'
     });
 
 })(jQuery);
